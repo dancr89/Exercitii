f201a6309517abf4a808f878fc2853850800c846
Dan Craciun <craciunmdan@yahoo.com>
2021-02-04 18:33:29 +0200

    exrcitii cu python github

---
+++ functions.py
@@ -0,0 +1,19 @@
+# my_var = 25
+# if my_var < 6:
+#     print('Set Instructini #1')
+# elif my_var < 10:
+#     print('Set Instructini #2')
+# elif my_var < 25:
+#     print('Set Instructini #3')
+# else:
+#     print('Set Instructini #4')
+#
+# print(type(my_var))
+
+mylist = [1,2,3, 202022]
+lenghtlist = len(mylist)
+
+if lenghtlist % 2 == 0:
+    print('Par')
+else:
+    print('Impar')
